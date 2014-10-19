9bdb33bc6f2f5431d572e74b9bef28d0eb6f1203
frozenm <frozenm@yandex.ru>
2014-10-19 21:44:10 +0400

    completed

--- class-06/06-bulls_n_cows.hs
+++ class-06/06-bulls_n_cows.hs
@@ -6,27 +6,36 @@
 import System.Environment
 import System.Random
 import Data.List
-
-type Bull = Int
-type Cow = Int
-
--- Проверяет, все ли цифры различны
-isDifferent :: Int -> Bool
-isDifferent number = ch0 /= ch1 && ch0 /= ch2 && ch0 /= ch3 && ch1 /= ch2 && ch1 /= ch3 && ch2 /= ch3
-  where
-    str = show number
-    ch0 = str !! 0
-    ch1 = str !! 1
-    ch2 = str !! 2
-    ch3 = str !! 3
+import Control.Monad (when)
 
 -- Задумываем число
 secretNumber :: (RandomGen g) => g -> Int
 secretNumber gen = head $ filter isDifferent $ randomRs (1000,9999) gen
+  where
+    isDifferent number = nub (show number) == show number
 
-rightAnswer :: String -> Stirng -> (Bull, Cow)
-rightAnswer numb1 numb2 = undefined
+-- Считаем быков и коров
+check :: String -> String -> (Int, Int)
+check numb1 numb2 = (bulls, cows)
+  where
+    bulls = length $ filter (\(x,y) -> x == y) $ zip numb1 numb2
+    cows = length (intersect numb1 numb2) - bulls 
+
+-- Играемся
+game :: Int -> IO ()
+game secret_number = do
+  numberString <- getLine
+  when (not $ null numberString) $ do
+  let number = read numberString 
+  let (bulls, cows) = check (show secret_number) numberString 
+  if secret_number == number
+    then putStrLn "You win!"
+    else do
+      putStrLn $ "Wrong! You have " ++ show bulls ++ " bulls and " ++ show cows ++ " cows"
+      game secret_number
 
 main = do
   gen <- newStdGen
-  putStrLn $ show $ secretNumber gen
+  let secret_number = secretNumber gen
+  putStrLn "I have a secret 4-digit number. What is it?"
+  game secret_number
