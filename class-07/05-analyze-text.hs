6f7f3b15933f35daacecd4f5d1291fd420206802
frozenm <frozenm@yandex.ru>
2014-10-27 22:08:28 +0400

    completed

--- class-07/05-analyze-text.hs
+++ class-07/05-analyze-text.hs
@@ -1,15 +1,35 @@
 {-
   Соберите следующую информацию по текстовому файлу kapitan.txt:
-
-  1) самый часто используемый знак препинания;
-  2) 50 наиболее часто используемых в тексте слов (с указанием количества использований);
-  3) частоты символов, биграмм и триграмм (вывести соответствующую информацию для
-     наиболее часто встречающихся);
-  4) количества использованных предлогов, местоимений и имён собственных.
-
   Постарайтесь использовать в решении наиболее подходящие структуры данных, избегайте повторные
   вычисления и заведомо неэффективные операции. Проверьте написанную программу на трёх других
   текстах того же или большего размера. Сравните результаты.
 -}
 
-main = undefined
+import Data.Char
+import Data.List
+import Data.Function (on)
+import qualified Data.Map as Map
+import System.Environment
+
+-- Загружаем текст в map
+textToMap :: (Ord a) => [a] -> Map.Map a Int 
+textToMap = foldl (\acc x -> Map.insertWith (+) x 1 acc) Map.empty
+
+-- 1) самый часто используемый знак препинания;
+mostFreqPunct:: String -> (Char, Int)
+mostFreqPunct text = maximumBy (compare `on` snd) $ Map.toList $ textToMap $ filter isPunctuation text
+     
+-- 2) 50 наиболее часто используемых в тексте слов;
+mostFreqWords :: String -> [(String, Int)]
+mostFreqWords text = take 50 $ sortBy (flip compare `on` snd) $ Map.toList $ textToMap $ filter (/= "") $ map (map toLower . filter (\x -> (isLetter x) || (x =='\''))) $ words text
+
+-- 3) частоты символов
+mostFreqChars :: String -> [(Char, Int)]
+mostFreqChars text = take 5 $ sortBy (flip compare `on` snd) $ Map.toList $ textToMap $ filter isLetter text
+
+main = do
+  (fname : _) <- getArgs
+  content <- readFile fname
+  putStrLn ("Most frequent punctuation:" ++ (show $ mostFreqPunct content))
+  putStrLn ("Most frequent words:" ++ (show $ mostFreqWords content))
+  putStrLn ("Most frequent chars:" ++ (show $ mostFreqChars content))
--- class-07/06-matrices.hs
+++ class-07/06-matrices.hs
@@ -1,9 +1,5 @@
 {-
    Напишите программу обработки квадратных матриц (на массивах) со следующими возможностями:
-   1) чтение матрицы из тестового файла;
-   2) запись матрицы в текстовый файл;
-   3) сумма матриц;
-   4) произведение матриц.
 
   Задание на обработку может выглядеть, к примеру, следующим образом (здесь вычисляется матричное
   выражение (A + B) * C):
@@ -17,3 +13,38 @@
 
    Параметром командной строки должно быть имя файла со сценарием указанного или подобного ему вида.
 -}
+import System.Environment
+import Data.Array.IArray
+import Data.List
+
+-- 1) чтение матрицы из тестового файла;
+readMatrix :: FilePath -> IO (Array (Int, Int) Int)
+readMatrix fname = do
+  content <- readFile fname
+  let matrixStr = lines content 
+  let n = length matrixStr
+  return $ listArray ((1, 1), (n, n)) $ (map read $ concat $ map words matrixStr)
+
+-- 2) запись матрицы в текстовый файл;
+writeMatrix :: Array (Int, Int) Int -> FilePath -> IO ()
+writeMatrix matrix fname = do
+  writeFile fname $ unlines $ map (tail . foldl (\acc x -> acc ++ " " ++ x) "") $ splitN (map show $ elems matrix) (fst $ snd $ bounds matrix)
+  where
+    splitN [] _ = []
+    splitN xs n = [take n xs] ++ (splitN (drop n xs) n)
+
+-- 3) сумма матриц;
+sumMatrix :: Array (Int, Int) Int -> Array (Int, Int) Int -> IO (Array (Int, Int) Int)
+sumMatrix x y = do
+  return $ array resultBounds [((r,c), x!(r,c) + y!(r,c)) | r <- range(lr, ur), c <- range(lc, uc)]
+  where
+    bx@((lr, lc), (ur, uc)) = bounds x
+    resultBounds
+      | bx == bounds y = bx
+      | otherwise = error "sumMatrix: incompatible bounds"
+
+main = do
+  m1 <- readMatrix "matrix.txt"
+  m2 <- readMatrix "matrix.txt"
+  sumM <- sumMatrix m1 m2
+  writeMatrix sumM "sum.txt"
